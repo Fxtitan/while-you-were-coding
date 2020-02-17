912fddfa5051f47fa3184c918bc6f41c466241cf
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-02-17 13:52:01 -0500

    did what I could

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
 
 // ##### Challenges #####
 
+
 // 1.
 //
 // Write a function that takes in a parameter and prints the string below, with the given paramter place of the $ symbol.
@@ -20,6 +21,17 @@
 // Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
 //
 // ###### Challenge Banner Function Works! #####
+function bruceBanner(num) {
+    console.log('#####' + 'Challenge' + num + '#####' );
+    }
+    // console.log(giveMe);
+    bruceBanner(1)
+//for (let i = 1; i < 5; i ++){
+    //console.log(i);
+//}
+
+
+
 
 
 // 2.
@@ -28,40 +40,75 @@
 
 // Use `if` and a modulus operation.
 
+for (let i = 1; i < 20; i += 3){
+    console.log(i); 
+}
+bruceBanner(2)
+
+
+
 
 // 3.
 //
 // Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
+for (let i = 1; i < 20; i += 3){
+    console.log(i);
+} bruceBanner(3);
 
 
 
 // 4.
 //
 // Console out the numbers from 1-20. BACKWARDS.
+for (let i = 20; i >= 0; i--) {
+console.log(i);
+} 
+bruceBanner(4);
 
 
 // 5.
 //
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
+let str = 'Boy howdy am I good at this!';
+for (let i = 0; i < str.length; i++) {
+    console.log(str.charAt(i));
+}
+bruceBanner(5);
+
+
 
 
 // 6.
 //
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
+str2 = 'And getting better every day.';
+for (let i = 1; i < str2.length; i+=3){
+console.log(str2.charAt(i));
+}
+bruceBanner(6);
 
 
 // 7. 
 //
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
+str3 = 'Am I the Best?'.toUpperCase();
+for(let i = 0; i < str3.length; i += 1 ){
+    console.log(str3.charAt(i));
+}
+bruceBanner(7);
 
 
 // 8.
 //
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
+str4 = 'Whoa, I'.toLowerCase() + 'AM'.toUpperCase() + 'the best!';
+for(let i = 0; i < str4.length; i += 1){
+console.log(str4.charAt(i))
+}
 
 
 // 9.
@@ -69,9 +116,17 @@
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
-
+function reverse(s) {
+    for ( i = s.length -1, o = ''; i >= 0; o += s [i--]) {}
+    return o;
+}
+console.log(reverse('I am become death, destroyer of worlds.'))
 
 // 10.
+
+let sharkString = "I am the one ";
+
+// Iterate through each index in the string
 //
 const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|Bogdan|Sharod|David|LaToddra|Carlos';
 
